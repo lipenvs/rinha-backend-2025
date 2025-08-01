1696d9daefd92716ef20fdb7a9f6434f1f18efe2
Felipe Neves <soufelipeneves@gmail.com>
2025-08-01 18:51:09 -0300

    create routes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    Router,
+    routing::{get, post},
+};
+
+mod controller;
+use controller::{get_summary, new_payment, purge_payments};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/payments", post(new_payment))
+        .route("/purge-payments", post(purge_payments))
+        .route("/payments-summary", get(get_summary));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
